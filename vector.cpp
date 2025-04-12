62084725acc22bbffb842156bd941d86e412f676
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2025-04-13 00:55:24 +0530

    vector

---
+++ vector.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+// //static
+// vector<int>v1={1,2,3,4,5,6,7,8,9,10};
+// for(auto p:v1){
+// cout<<p<<" ";
+// }
+// cout<<"\n";
+
+//     //dynamic
+// vector<int>v;
+// v.push_back(1);
+// cout<<"Size "<<v.size()<<endl;
+// cout<<"Capacity "<<v.capacity()<<endl;
+// cout<<"max size "<<v.max_size()<<endl;
+// v.push_back(2);
+// cout<<"Size "<<v.size()<<endl;
+// cout<<"Capacity "<<v.capacity()<<endl;
+// cout<<"max size "<<v.max_size()<<endl;
+// v.push_back(3);
+// cout<<"Size "<<v.size()<<endl;
+// cout<<"Capacity "<<v.capacity()<<endl; 
+// v.push_back(4);
+// cout<<"Size "<<v.size()<<endl;
+// cout<<"Capacity "<<v.capacity()<<endl;
+// v.push_back(5);
+// cout<<"Size "<<v.size()<<endl;
+// cout<<"Capacity "<<v.capacity()<<endl;
+// v.push_back(7);
+// cout<<"Size "<<v.size()<<endl;
+// cout<<"Capacity "<<v.capacity()<<endl;
+// v.push_back(8);
+// cout<<"Size "<<v.size()<<endl;
+// cout<<"Capacity "<<v.capacity()<<endl;
+
+
+// for(auto p:v){
+// cout<<p<<" ";
+// }
+
+
+
+
+
+
+vector<int>t(3);
+t[0]=1;
+t[1]=2;
+t[2]=3;
+for(auto p:t){
+    cout<<p<<" ";   
+}
+
+}
